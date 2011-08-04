df55856fa446b986e2f6afd1355974656ddc58bf
ncherro <ncherro@gmail.com>
2011-08-04 10:28:09 -0400

    updated server-install

--- server-install.sh
+++ server-install.sh
@@ -1,4 +1,4 @@
-mv ~/.bashrc ~/.bashrc-bak;
+test -r ~/.bashrc && mv ~/.bashrc ~/.bashrc-bak;
 ln -s ./bashrc-for-linux-servers ~/.bashrc;
-touch ~/.bashrc.local;
+test -r ~/.bashrc.local || echo "# use this file to extend ~/.bashrc, which is a symlink" > ~/.bashrc.local;
 echo "DONE!"
