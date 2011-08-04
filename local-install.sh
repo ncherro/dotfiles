1b803b0b7329143520a97e160e05cb71e98194aa
ncherro <ncherro@gmail.com>
2011-08-04 10:57:56 -0400

    updated installation scripts

--- local-install.sh
+++ local-install.sh
@@ -1,5 +1,11 @@
+echo "Symlinking rc files"
 test -r ~/.gvimrc.local && mv ~/.gvimrc.local ~/.gvimrc.local.bak;
 test -r ~/.vimrc.local && mv ~/.vimrc.local ~/.vimrc.local.bak;
 test -r ~/.janus.rake && mv ~/.janus.rake ~/.janus.rake.bak;
-ln -s ./gvimrc.local ~/.gvimrc.local; ln -s ./vimrc.local ~/.vimrc.local; ln -s ./janus.rake ~/.janus.rake;
+ln -s `pwd`/gvimrc.local ~/.gvimrc.local; ln -s `pwd`/vimrc.local ~/.vimrc.local; ln -s `pwd`/janus.rake ~/.janus.rake;
+echo "Setting up git aliases"
+git config --global alias.co checkout;
+git config --global alias.br branch;
+git config --global alias.ci commit;
+git config --global alias.st status;
 echo "DONE!"
--- server-install.sh
+++ server-install.sh
@@ -1,3 +1,4 @@
+echo "Symlinking rc files, setting up vim"
 test -r ~/.bashrc && mv ~/.bashrc ~/.bashrc.bak;
 ln -s ./bashrc-for-linux-servers ~/.bashrc;
 test -r ~/.bashrc.local || echo "# use this file to extend ~/.bashrc, which is symlinked to bashrc-for-linux-servers in the dotfiles directory" > ~/.bashrc.local;
@@ -5,4 +6,11 @@ test -r ~/.vim && mv ~/.vim ~/.vim.bak
 ln -s vim ~/.vim
 test -r ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
 ln -s vimrc ~/.vimrc
+
+echo "Setting up git aliases"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
 echo "DONE!"
