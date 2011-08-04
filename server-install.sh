6f2467310f5a3baa570d333e4a0a7d55b43b3f3e
ncherro <ncherro@gmail.com>
2011-08-04 10:35:39 -0400

    updated server-install

--- server-install.sh
+++ server-install.sh
@@ -1,4 +1,8 @@
-test -r ~/.bashrc && mv ~/.bashrc ~/.bashrc-bak;
+test -r ~/.bashrc && mv ~/.bashrc ~/.bashrc.bak;
 ln -s ./bashrc-for-linux-servers ~/.bashrc;
-test -r ~/.bashrc.local || echo "# use this file to extend ~/.bashrc, which is a symlink" > ~/.bashrc.local;
+test -r ~/.bashrc.local || echo "# use this file to extend ~/.bashrc, which is symlinked to bashrc-for-linux-servers in the dotfiles directory" > ~/.bashrc.local;
+test -r ~/.vim && mv ~/.vim ~/.vim.bak
+ln -s vim ~/.vim
+test -r ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
+ln -s vimrc ~/.vimrc
 echo "DONE!"
