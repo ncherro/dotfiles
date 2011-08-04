78d8ad6358d2ffdad7cac67ce58140a83dde8fe5
ncherro <ncherro@gmail.com>
2011-08-04 10:29:48 -0400

    removed dots from files

--- local-install.sh
+++ local-install.sh
@@ -1,3 +1,5 @@
-mv ~/.gvimrc.local ~/.gvimrc.local.bak; mv ~/.vimrc.local ~/.vimrc.local.bak; mv ~/.janus.rake ~/.janus.rake.bak;
-ln -s ./.gvimrc.local ~/.gvimrc.local; ln -s ./.vimrc.local ~/.vimrc.local; ln -s ./.janus.rake ~/.janus.rake;
+test -r ~/.gvimrc.local && mv ~/.gvimrc.local ~/.gvimrc.local.bak;
+test -r ~/.vimrc.local && mv ~/.vimrc.local ~/.vimrc.local.bak;
+test -r ~/.janus.rake && mv ~/.janus.rake ~/.janus.rake.bak;
+ln -s ./gvimrc.local ~/.gvimrc.local; ln -s ./vimrc.local ~/.vimrc.local; ln -s ./janus.rake ~/.janus.rake;
 echo "DONE!"
