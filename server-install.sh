641d5818eed7a59a3306dc9223f4c229a69d81bc
ncherro <ncherro@gmail.com>
2011-08-04 10:58:49 -0400

    x

--- server-install.sh
+++ server-install.sh
@@ -1,11 +1,11 @@
 echo "Symlinking rc files, setting up vim"
 test -r ~/.bashrc && mv ~/.bashrc ~/.bashrc.bak;
-ln -s ./bashrc-for-linux-servers ~/.bashrc;
+ln -s `pwd`/bashrc-for-linux-servers ~/.bashrc;
 test -r ~/.bashrc.local || echo "# use this file to extend ~/.bashrc, which is symlinked to bashrc-for-linux-servers in the dotfiles directory" > ~/.bashrc.local;
 test -r ~/.vim && mv ~/.vim ~/.vim.bak
-ln -s vim ~/.vim
+ln -s `pwd`/vim ~/.vim
 test -r ~/.vimrc && mv ~/.vimrc ~/.vimrc.bak
-ln -s vimrc ~/.vimrc
+ln -s `pwd`/vimrc ~/.vimrc
 
 echo "Setting up git aliases"
 git config --global alias.co checkout
