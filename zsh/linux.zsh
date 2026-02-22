2b3c3181c4a5cf63f1b169d5a58df935b7fb9cfc
Nick Herro <ncherro@gmail.com>
2026-02-22 09:14:05 -0600

    adds ghopen to linux

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -11,5 +11,8 @@ eval "$(keychain --eval --quiet ~/.ssh/id_ed25519)"
 # --- PATH ---
 export PATH="$HOME/.local/bin:$PATH"
 
+# --- GitHub ---
+alias ghopen="git config --get remote.origin.url | sed 's/:/\//' | sed 's/git@/https:\/\//' | sed 's/\.git//' | xargs xdg-open"
+
 # --- Misc ---
 export LD_LIBRARY_PATH=/home/ebenezer/workspace/vid.stab:$LD_LIBRARY_PATH
